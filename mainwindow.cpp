542d67c2806e18af53a38df7a321a03307d7ba2f
John Joseph Afaga <afagaj@spu.edu>
2020-03-16 20:27:02 -0700

    added a decent amount of stuff

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -33,129 +33,8 @@ void MainWindow::on_taskIcon_clicked()
     ui->stackedWidget->setCurrentIndex(2);
 }
 
-void MainWindow::on_taskDate1_userDateChanged(const QDate &date)
-{
-
-    date1 = date;
-}
-
-void MainWindow::on_taskDate2_userDateChanged(const QDate &date)
-{
-    date2 = date;
-}
-
-void MainWindow::on_taskDate3_userDateChanged(const QDate &date)
-{
-    date3 = date;
-}
-
-void MainWindow::on_taskDate4_userDateChanged(const QDate &date)
-{
-    date4 = date;
-}
-
-void MainWindow::on_taskDate5_userDateChanged(const QDate &date)
-{
-    date5 = date;
-}
-
 void MainWindow::on_submitTaskButton_clicked()
 {
-    //gather the tasks from the user and put them in a string
-//    task1 = ui->taskLine1->text();
-//    task2 = ui->taskLine2->text();
-//    task3 = ui->taskLine3->text();
-//    task4 = ui->taskLine4->text();
-//    task5 = ui->taskLine5->text();
-    QString taskclear = "";
-
-
-
-    //clear the current tasks
-//    ui->taskLabel1->setText(taskclear);
-//    ui->taskLabel2->setText(taskclear);
-//    ui->taskLabel3->setText(taskclear);
-//    ui->taskLabel4->setText(taskclear);
-//    ui->taskLabel5->setText(taskclear);
-
-
-
-
-    //make it so the tasks are always ordered from top to bottom
-    if (task1.length() == 0)
-    {
-        if(task2.length() != 0)
-        {
-            task1 = task2;
-            task2 = task3;
-            task3 = task4;
-            task4 = task5;
-            task5 = taskclear;
-        }
-        else if(task3.length() != 0)
-        {
-            task1 = task3;
-            task3 = task4;
-            task4 = task5;
-            task5 = taskclear;
-        }
-        else if(task4.length() != 0)
-            {
-            task1 = task4;
-            task4 = task5;
-            task5 = taskclear;
-            }
-        else if(task5.length() != 0)
-            task1 = task5;
-            task5 = taskclear;
-    }
-
-    if (task2.length() == 0)
-    {
-        if(task3.length() != 0)
-        {
-            task2 = task3;
-            task3 = task4;
-            task4 = task5;
-            task5 = taskclear;
-        }
-        else if(task4.length() != 0)
-            {
-            task2 = task4;
-            task4 = task5;
-            task5 = taskclear;
-            }
-        else if(task5.length() != 0)
-            task2 = task5;
-            task5 = taskclear;
-    }
-
-    if (task3.length() == 0)
-    {
-        if(task4.length() != 0)
-           {
-            task3 = task4;
-            task4 = task5;
-            task5 = taskclear;
-           }
-        else if(task5.length() != 0)
-            task3 = task5;
-            task5 = taskclear;
-    }
-    if (task4.length() == 0)
-    {
-     if(task5.length() != 0)
-            task4 = task5;
-            task5 = taskclear;
-    }
-
-//paste the tasks
-    ui->taskLabel1->setText(task1);
-    ui->taskLabel2->setText(task2);
-    ui->taskLabel3->setText(task3);
-    ui->taskLabel4->setText(task4);
-    ui->taskLabel5->setText(task5);
-
     this->dueDateChecker();
 }
 
@@ -187,46 +66,67 @@ void MainWindow::setCurrentDate()
 // sends message box if so
 void MainWindow::dueDateChecker()
 {
-    QString dueTasks = "";
+    QString dueTasks = "Tasks Due: \n";
+    QString dueHomework = "Homework Due: \n";
 
-    if(task1 != "" && date1 <= curDate)
-        dueTasks += task1 + " is due!\n";
+    for (int i = 0; i < tasks.length(); i++)
+    {
+        if(taskDates[i] < curDate)
+        {
+            dueTasks += tasks[i] + "\n";
+        }
+    }
 
-    if(task2 != "" && date2 <= curDate)
-        dueTasks += task2 + " is due!\n";
+    for (int i = 0; i < homeworks.length(); i++)
+    {
+        if(homeworkDates[i] < curDate)
+        {
+            dueHomework += homeworks[i] + "\n";
+        }
+    }
 
-    if(task3 != "" && date3 <= curDate)
-        dueTasks += task3 + " is due!\n";
-
-    if(task4 != "" && date4 <= curDate)
-        dueTasks += task4 + " is due!\n";
-
-    if(task5 != "" && date5 <= curDate)
-        dueTasks += task5 + " is due!\n";
-
-    if(dueTasks != "")
+    if(dueTasks != "Tasks Due: \n")
     {
         msgBox.setText(dueTasks);
         msgBox.exec();
     }
-}
 
+    if(dueHomework != "Homework Due: \n")
+    {
+        msgBox.setText(dueHomework);
+        msgBox.exec();
+    }
 
-void MainWindow::on_TEST_clicked()
-{
-//    tasks.removeLast();
-//    ui->taskLabel5->setText(tasks.last());
-    tasks.append(ui->taskTitle->text());
-    ui->taskLabel4->setText(tasks.last());
 }
 
 void MainWindow::on_taskAdd_clicked()
 {
     if(!ui->taskTitle->text().isEmpty())
     {
-        tasks.append(ui->taskTitle->text());
-        ui->taskLabel4->setText(tasks.last());
+        QString title = ui->taskTitle->text();
+        QDate date = ui->taskDate->date();
+        tasks.append(title);
+        taskDates.append(date);
+
+        if(ui->taskLayout->count() < 5)
+        {
+            QString label = title + "\t" + ui->taskDate->text();
+            QLabel *task = new QLabel(label);
+            task->setFixedWidth(300);
+            task->setScaledContents(true);
+            ui->taskLayout->addWidget(task);
+            ui->taskLayout->setAlignment(Qt::AlignTop);
+        }
+
+        QString display = title + "\t" + ui->taskDate->text();
+        QLabel *taskDisplay = new QLabel(display);
+        taskDisplay->setFixedWidth(300);
+        taskDisplay->setScaledContents(true);
+        ui->taskFullLayout->addWidget(taskDisplay);
+        ui->taskFullLayout->setAlignment(Qt::AlignTop);
     }
+
+    this->dueDateChecker();
 }
 
 void MainWindow::on_homeworkAdd_clicked()
@@ -234,7 +134,10 @@ void MainWindow::on_homeworkAdd_clicked()
     if(!ui->homeworkTitle->text().isEmpty())
     {
         QString title = ui->homeworkTitle->text();
+        QDate date = ui->homeworkDate->date();
         homeworks.append(title);
+        homeworkDates.append(date);
+
         if(ui->homeworkLayout->count() < 5)
         {
             QString label = title + "\t" + ui->homeworkDate->text();
@@ -242,6 +145,16 @@ void MainWindow::on_homeworkAdd_clicked()
             homework->setFixedWidth(300);
             homework->setScaledContents(true);
             ui->homeworkLayout->addWidget(homework);
+            ui->homeworkLayout->setAlignment(Qt::AlignTop);
         }
+
+        QString display = title + "\t" + ui->homeworkDate->text();
+        QLabel *hwDisplay = new QLabel(display);
+        hwDisplay->setFixedWidth(300);
+        hwDisplay->setScaledContents(true);
+        ui->hwFullLayout->addWidget(hwDisplay);
+        ui->hwFullLayout->setAlignment(Qt::AlignTop);
     }
+
+        this->dueDateChecker();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -26,16 +26,6 @@ private slots:
 
     void on_taskIcon_clicked();
 
-    void on_taskDate1_userDateChanged(const QDate &date);
-
-    void on_taskDate2_userDateChanged(const QDate &date);
-
-    void on_taskDate3_userDateChanged(const QDate &date);
-
-    void on_taskDate4_userDateChanged(const QDate &date);
-
-    void on_taskDate5_userDateChanged(const QDate &date);
-
     void on_submitTaskButton_clicked();
 
     void on_addButton_clicked();
@@ -44,29 +34,21 @@ private slots:
 
     void dueDateChecker();
 
-    void on_TEST_clicked();
-
     void on_taskAdd_clicked();
 
     void on_homeworkAdd_clicked();
 
 private:
     Ui::MainWindow *ui;
-    QString task1;
-    QString task2;
-    QString task3;
-    QString task4;
-    QString task5;
+
     QString dateString;
+
     QVector <QString> homeworks;
+    QVector <QDate> homeworkDates;
     QVector <QString> tasks;
+    QVector <QDate> taskDates;
 
     QDate curDate;
-    QDate date1;
-    QDate date2;
-    QDate date3;
-    QDate date4;
-    QDate date5;
 
     QMessageBox msgBox;
 };
